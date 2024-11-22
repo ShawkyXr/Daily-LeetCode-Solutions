218aeffe1e28ba83b731175dc85309c01d042acf
Ahmed Shawky <130262201+ShawkyXr@users.noreply.github.com>
2024-11-22 14:51:41 +0200

    [1072] [Time Beats: 85.51%] [Memory Beats: 58.96%] - LeetPush

--- 1072-Flip-Columns-For-Maximum-Number-of-Equal-Rows.cpp
+++ 1072-Flip-Columns-For-Maximum-Number-of-Equal-Rows.cpp
@@ -2,16 +2,16 @@ class Solution {
 public:
     int maxEqualRowsAfterFlips(vector<vector<int>>& matrix) {
         int ans =0;
-        for (int i=0 ;i<matrix.size()-1 ;i++){
-            int val=1;
-            for (int k=i+1 ; k<matrix.size() ; k++){
-                    int sum =0 ;
-                for (int j=0 ; j<matrix[0].size() ; j++){
-                    sum+= (matrix[i][j]^matrix[k][j]);
-                }
-                if (sum==0 || sum==matrix[0].size()) val++;
+        map<string,int> mp;
+        for (int i=0 ;i<matrix.size() ;i++){
+            string s="";
+            for (int j=0 ;j<matrix[0].size() ;j++){
+                s+= (matrix[i][j]^matrix[i][0])+'0';
             }
-            ans = max(ans,val);
+            mp[s]++;
+        }
+        for (auto f : mp){
+            ans = max(f.second,ans);
         }
         return ans;
     }
