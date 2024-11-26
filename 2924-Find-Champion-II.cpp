81f388137795bc47d2d109750b4c083b1424d0ad
Ahmed Shawky <130262201+ShawkyXr@users.noreply.github.com>
2024-11-26 12:58:53 +0200

    [2924] [Time Beats: 100.00%] [Memory Beats: 66.75%] - LeetPush

--- 2924-Find-Champion-II.cpp
+++ 2924-Find-Champion-II.cpp
@@ -2,17 +2,17 @@ class Solution {
 public:
     int findChampion(int n, vector<vector<int>>& edges) {
         vector <int> loser(n,0);
-        for (auto edge : edges){
+        for (auto &edge : edges){
             loser[edge[1]]++;
         }
         int cnt = 0,ans=0;
         for (int i=0 ;i<n ;i++){
             if (!loser[i]){
+                if (cnt!=0) return -1;
                 cnt++;
                 ans=i;
             }
         }
-        if (cnt!=1) return -1;
         return ans;
     }
 };
