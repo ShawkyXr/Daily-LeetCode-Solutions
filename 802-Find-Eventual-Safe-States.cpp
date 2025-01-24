90ee34fb343d791c8e66b78e18bc0ee9db4d8c31
Ahmed Shawky <130262201+ShawkyXr@users.noreply.github.com>
2025-01-24 15:09:00 +0200

    [802] [Time Beats: 38.48%] [Memory Beats: 63.34%] - LeetPush

--- 802-Find-Eventual-Safe-States.cpp
+++ 802-Find-Eventual-Safe-States.cpp
@@ -1,35 +1,33 @@
 class Solution {
-        map <int,int> term,safe;
+        map <int,int> safe;
 
         int dfs(int u,vector<vector<int>>& graph,vector <int> &vis){
-            if (safe.count(u) || term.count(u)) {
+            if (safe.count(u) ) {
                 safe[u] = 1;
                 return 1;
             }
             if (vis[u]) return 0;
             vis[u] = 1;
-            int ans = 1;
             for (auto v: graph[u]){
                 if (dfs(v,graph,vis)){
                     safe[v] = 1;
-                    ans &= 1;
                 }else{
                     return 0;
                 }
             } 
-            return ans;
+            return 1;
         }
 
 public:
     vector<int> eventualSafeNodes(vector<vector<int>>& graph) {
         int n = graph.size();
         for (int i=0 ; i<n ; i++){
-            if (graph[i].size()==0) term[i] = 1;
+            if (graph[i].size()==0) safe[i] = 1;
         }
         
         vector <int> ans;
+        vector <int> vis(n,0);
         for (int i=0 ; i<n ;i++){
-            vector <int> vis(n,0);
             if (dfs(i,graph,vis)) {
                 ans.push_back(i);
                 safe[i] = 1;    
