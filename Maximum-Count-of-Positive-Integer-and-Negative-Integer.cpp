1bc771d81c56aa65a2a1da3f0e8d44df151cdcc7
Ahmed Shawky <130262201+ShawkyXr@users.noreply.github.com>
2025-03-12 17:33:21 +0200

    [2529] [Time Beats: 100.00%] [Memory Beats: 75.85%] - LeetPush

--- Maximum-Count-of-Positive-Integer-and-Negative-Integer.cpp
+++ Maximum-Count-of-Positive-Integer-and-Negative-Integer.cpp
@@ -1,11 +1,10 @@
 class Solution {
 public:
     int maximumCount(vector<int>& nums) {
-        int neg=0,pos=0;
-        for (auto &n : nums){
-            if (n<0) neg++;
-            if (n>0) pos++;
-        }
-        return max(neg,pos);
+        int neg= lower_bound(nums.begin(),nums.end(),0) - nums.begin();
+        int pos= lower_bound(nums.begin(),nums.end(),1) - nums.begin();
+        int Nneg = max(0,neg);
+        int Npos = max(0,int(nums.size())-pos);
+        return max(Npos,Nneg); 
     }
 };
