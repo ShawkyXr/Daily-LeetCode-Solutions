5308352a20efc5ef3494a9a7ab6c200bae75f24f
Ahmed Shawky <130262201+ShawkyXr@users.noreply.github.com>
2025-05-08 17:12:06 +0300

    [3342] [Time Beats: 46.62%] [Memory Beats: 44.09%] - LeetPush

---
+++ Find-Minimum-Time-to-Reach-Last-Room-II.cpp
@@ -0,0 +1,48 @@
+class Solution {
+    int dx[4] = {1,0,-1,0};
+    int dy[4] = {0,1,0,-1};
+
+    struct room{
+        long long x,y,t;
+        int move;
+
+        room(long long x,long long y, long long t, int move): x(x), y(y), t(t), move(move){}
+
+        bool operator < (const room& e) const{
+            return t > e.t;
+        }
+    };
+
+public:
+    int minTimeToReach(vector<vector<int>>& moveTime) {
+        int n = moveTime.size(),m = moveTime[0].size();
+        priority_queue <room> pq;
+        vector <vector <long long>> dis(n,vector <long long>(m,100000000005));
+        vector <vector <int>> vis(n,vector <int>(m,0));
+
+        pq.push(room(0,0,0,0));
+        dis[0][0] = 0;
+        while(!pq.empty()){
+            room r = pq.top();
+            pq.pop();
+
+            if (vis[r.x][r.y]) continue;
+            vis[r.x][r.y] = 1;
+            
+            for (int i=0 ; i<4 ; i++){
+                int x = r.x+dx[i], y = r.y+dy[i];
+                if (x>=0 && x<n && y>=0 && y<m){
+                    long long t = max(dis[r.x][r.y],(long long)moveTime[x][y]) ;
+                    if (r.move%2==0) t+=1;
+                    else t+=2;
+                    if (t < dis[x][y]){
+                        dis[x][y] =  t;
+                        pq.push(room(x,y,t,r.move+1));
+                    }
+                }
+            }          
+        }
+
+        return dis[n-1][m-1];
+    }
+};
